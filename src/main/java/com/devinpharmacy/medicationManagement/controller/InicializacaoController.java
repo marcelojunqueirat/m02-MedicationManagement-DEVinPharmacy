f83b2d266984efa07515d99e89960177250e9af0
Marcelo Teixeira <marcelojxt@gmail.com>
2023-11-29 20:42:31 -0300

    feat: Criando carga inicial para Estoques

--- src/main/java/com/devinpharmacy/medicationManagement/controller/InicializacaoController.java
+++ src/main/java/com/devinpharmacy/medicationManagement/controller/InicializacaoController.java
@@ -50,6 +50,21 @@ public class InicializacaoController {
             medicamentoService.salvar(medicamento5);
             medicamentoService.salvar(medicamento6);
         }
+        var estoques = estoqueService.consultar();
+        if(estoques.isEmpty()){
+            Estoque estoque1 = new Estoque(90561736000121L, 1010, 12, LocalDateTime.now());
+            Estoque estoque2 = new Estoque(90561736000121L, 7473, 10, LocalDateTime.now());
+            Estoque estoque3 = new Estoque(43178995000198L, 7473, 2, LocalDateTime.now());
+            Estoque estoque4 = new Estoque(43178995000198L, 2233, 15, LocalDateTime.now());
+            Estoque estoque5 = new Estoque(43178995000198L, 8880, 16, LocalDateTime.now());
+            Estoque estoque6 = new Estoque(43178995000198L, 4040, 22, LocalDateTime.now());
+            estoqueService.salvar(estoque1);
+            estoqueService.salvar(estoque2);
+            estoqueService.salvar(estoque3);
+            estoqueService.salvar(estoque4);
+            estoqueService.salvar(estoque5);
+            estoqueService.salvar(estoque6);
+        }
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/devinpharmacy/medicationManagement/service/EstoqueService.java
+++ src/main/java/com/devinpharmacy/medicationManagement/service/EstoqueService.java
@@ -1,11 +1,34 @@
 package com.devinpharmacy.medicationManagement.service;
 
+import com.devinpharmacy.medicationManagement.exception.RegistroJaExistenteException;
+import com.devinpharmacy.medicationManagement.model.Estoque;
+import com.devinpharmacy.medicationManagement.model.IdEstoque;
 import com.devinpharmacy.medicationManagement.repository.EstoqueRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EstoqueService {
     @Autowired
     private EstoqueRepository estoqueRepo;
+
+    @Transactional
+    public List<Estoque> consultar() {
+        return estoqueRepo.findAll();
+    }
+
+    @Transactional
+    public Estoque salvar(Estoque estoque) {
+        boolean existe = estoqueRepo.existsById(new IdEstoque(estoque.getCnpj(), estoque.getNroRegistro()));
+        if (existe) {
+            throw new RegistroJaExistenteException("Estoque",
+                    String.format("CNPJ: %s, Número do Registro: %s.", estoque.getCnpj(), estoque.getNroRegistro())
+            );
+        }
+        estoque = estoqueRepo.save(estoque);
+        return estoque;
+    }
 }
