2deaaabde5d22c9d23f57a776668bbc3fbb93408
Marcelo Teixeira <marcelojxt@gmail.com>
2023-11-29 21:10:47 -0300

    feat: Implementado endpoint de consulta de Farmacia

---
+++ src/main/java/com/devinpharmacy/medicationManagement/controller/FarmaciaController.java
@@ -0,0 +1,36 @@
+package com.devinpharmacy.medicationManagement.controller;
+
+import com.devinpharmacy.medicationManagement.dto.FarmaciaResponse;
+import com.devinpharmacy.medicationManagement.model.Farmacia;
+import com.devinpharmacy.medicationManagement.service.FarmaciaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/farmacias")
+public class FarmaciaController {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private FarmaciaService farmaciaService;
+
+    @GetMapping
+    public ResponseEntity<List<FarmaciaResponse>> consultar() {
+        var farmacias = farmaciaService.consultar();
+        var resp = new ArrayList<FarmaciaResponse>();
+        for (Farmacia farmacia : farmacias) {
+            var farmaciaDTO = modelMapper.map(farmacia, FarmaciaResponse.class);
+            resp.add(farmaciaDTO);
+        }
+
+        return ResponseEntity.ok(resp);
+    }
+}
